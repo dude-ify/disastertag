ca7ea2772a9e74ffe1a3578b1eb7b60a7cbe9089
Connor Jones <cj@cjmakes.com>
2018-01-20 04:48:27 -0500

    unbroke the api urls

--- src/api/urls.py
+++ src/api/urls.py
@@ -4,8 +4,8 @@ from .views import CreateView
 from .views import DetailsView
 
 urlpatterns = {
-        url(r'^/api/v1/dtags/$', CreateView.as_view(), name="create"),
-        url(r'^d/api/v1/tags/(?P<pk>[0-9]+)/$',
+        url(r'^api/v1/dtags/$', CreateView.as_view(), name="create"),
+        url(r'^api/v1/dtags/(?P<pk>[0-9]+)/$',
             DetailsView.as_view(), name="details"),
         }
 
