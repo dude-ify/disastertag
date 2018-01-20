5b8830a8db04daa4a46c8064907ba49fb840d32a
AmritHariharan <stuka241@gmail.com>
2018-01-20 10:24:26 -0500

    working on getting modelform to work in view

--- src/dtags/views.py
+++ src/dtags/views.py
@@ -3,31 +3,32 @@ from django.shortcuts import render
 from dtags.models import Dtag
 
 class Landing:
-    def index(request):
-        return render(request, 'dtags/index.html')
+	def index(request):
+		return render(request, 'dtags/index.html')
 
 class Create:
-    def index(request):
-        if request.method == 'GET': # what the fuck is this for?
-            return render(request, 'dtags/create.html')
+	def index(request):
+		if request.method == 'GET': # what the fuck is this for?
+			return render(request, 'dtags/create.html')
+		if request.method == 'POST':
+			form = DtagForm(request.POST)
+			if form.is_valid():
+				# Should do some validation here ...
+				dtag_temp = form.save(commit=False)
+			else:
+				form = DtagForm()
+			return render_to_response('dtags/create.html', {'DtagForm': form})
 
-        if request.method == 'POST':
-            form = DtagForm(request.POST)
-            # Should do some validation here ...
-            #dtag = form.save()
-            #form = DtagForm()
-            #return HttpResponseRedirect(reverse('create'))
-            return render_to_response('dtags/create.html', {'DtagForm': form})
 
 
 class Read:
-    def index(request):
-        all_patients = Dtag.objects.order_by('severity', '-date_created')
-        cxt = {"patients": all_patients}
-        return render(request, 'dtags/read.html', cxt)
+	def index(request):
+		all_patients = Dtag.objects.order_by('severity', '-date_created')
+		cxt = {"patients": all_patients}
+		return render(request, 'dtags/read.html', cxt)
 
-    def info(request, patient_id):
-        patient = get_object_or_404(barcode_id=patient_id)
-        cxt = {"patient": patient}
-        return render(request, 'dtags/patient_info.html', cxt)
+	def info(request, patient_id):
+		patient = get_object_or_404(barcode_id=patient_id)
+		cxt = {"patient": patient}
+		return render(request, 'dtags/patient_info.html', cxt)
 
