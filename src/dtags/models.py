4666cf2dfc2e3514e1616aabc80727b70eb918c2
Connor Jones <cj@cjmakes.com>
2018-01-20 05:56:29 -0500

    added model for dtags

---
+++ src/dtags/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Dtag(models.Model):
+    barcode = models.PositiveIntegerField(default=1, unique=True)
+    first_name = models.CharField(max_length=225, blank=True)
+    last_name = models.CharField(max_length=225, blank=True)
+    notes = models.TextField(blank=True)
+    loc_lat = models.FloatField(blank=False)
+    loc_lon = models.FloatField(blank=False)
+    SEVERITY_CHOICES = (
+            ('SR', 'Severe'),
+            ('MO', 'Moderate'),
+            ('MD', 'Mild'),
+            )
+    severity = models.CharField(
+            max_length=2,
+            choices=SEVERITY_CHOICES,
+            default='SR',
+            )
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return a human readable rep of the model instance"""
+        return "{}".format(self.name)
